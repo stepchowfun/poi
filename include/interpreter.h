28a30296520b4d75c66b952d2b83825c9185a5e6
Stephan Boyer <stephan@stephanboyer.com>
2017-08-19 20:55:03 -0700

    Implement the shell of an interpreter

--- include/interpreter.h
+++ include/interpreter.h
@@ -6,9 +6,15 @@
 #define POI_INTERPRETER_H
 
 #include <poi/instruction.h>
-#include <poi/string_pool.h>
+#include <poi/value.h>
+#include <vector>
 
 namespace Poi {
+  Value * interpret(
+    Poi::Instruction *program,
+    size_t program_size,
+    size_t start
+  );
 }
 
 #endif
--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -1,1 +1,9 @@
 #include <poi/interpreter.h>
+
+Poi::Value * Poi::interpret(
+  Poi::Instruction *program,
+  size_t program_size,
+  size_t start
+) {
+  return nullptr;
+}
